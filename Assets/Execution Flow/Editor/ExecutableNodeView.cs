2b38896db389f1067ad25ca250113f86bb0dc105
Chase McManning <mcmanning.1@osu.edu>
2020-09-19 09:08:54 -0400

    New project for BlueGraph samples

---
+++ Assets/Execution Flow/Editor/ExecutableNodeView.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEngine.UIElements;
+using BlueGraph;
+using BlueGraph.Editor;
+
+namespace BlueGraphSamples
+{
+    [CustomNodeView(typeof(ExecutableNode))]
+    [CustomNodeView(typeof(EventNode))]
+    public class ExecutableNodeView : NodeView
+    {
+        protected override void OnInitialize()
+        {
+            styleSheets.Add(Resources.Load<StyleSheet>("BlueGraphSamples/ExecutableNodeView"));
+            AddToClassList("executableNodeView");
+
+            if (Target is EventNode)
+            {
+                AddToClassList("eventNodeView");
+            }
+
+            // Customize placement of the default exec IO ports 
+            PortView inView = GetInputPort("ExecIn");
+            PortView outView = GetOutputPort("ExecOut");
+
+            if (inView != null) inView.AddToClassList("execInPortView");
+            
+            if (outView != null) outView.AddToClassList("execOutPortView");
+        }
+    }
+}
---
+++ Assets/Terrain Generator/Scripts/ProceduralTerrain.cs
@@ -0,0 +1,77 @@
+﻿
+using UnityEngine;
+
+namespace BlueGraphSamples
+{
+    [RequireComponent(typeof(Terrain))]
+    public class ProceduralTerrain : MonoBehaviour
+    {
+        public TerrainGraph graph;
+
+        /// <summary>
+        /// Create a Unity Terrain, execute the graph to generate the heightmap,
+        /// and assign the result to the Terrain.
+        /// </summary>
+        void Start()
+        {
+            Execute();
+        }
+
+        /// <summary>
+        /// Execute the graph and apply to the Unity Terrain
+        /// </summary>
+        public void Execute()
+        {
+            if (!graph)
+            {
+                Debug.LogError("ProceduralTerrain needs a Graph to execute");
+                return;
+            }
+
+            var terrain = GetComponent<Terrain>();
+            var data = terrain.terrainData;
+
+            var resolution = data.heightmapResolution;
+            var fres = (float)resolution;
+            
+            #if UNITY_EDITOR
+            UnityEditor.EditorUtility.DisplayProgressBar("Generate Heightmap", "Executing Graph", 0);
+            #endif 
+
+            var heightmap = graph.GetOutputHeightmap();
+            if (heightmap == null)
+            {
+                Debug.LogError("No output Heightmap");
+                return;
+            }
+
+            // TODO: Faster.
+            var heights = data.GetHeights(0, 0, resolution, resolution);
+
+            for (int y = 0; y < resolution; y++)
+            {
+                #if UNITY_EDITOR
+                if (y % 20 == 0)
+                {
+                     UnityEditor.EditorUtility.DisplayProgressBar(
+                         "Generate Heightmap", 
+                         "Creating heightmap texture", 
+                         Mathf.InverseLerp(0.0f, fres, y)
+                     );
+                }
+                #endif
+
+                for (int x = 0; x < resolution; x++)
+                {
+                    heights[y, x] = heightmap.GetHeightBilinear(x / fres, y / fres);
+                }
+            }
+            
+            #if UNITY_EDITOR
+            UnityEditor.EditorUtility.ClearProgressBar();
+            #endif 
+
+            data.SetHeights(0, 0, heights);
+        }
+    }
+}
